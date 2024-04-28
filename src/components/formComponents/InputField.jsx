e1523abcbb6fcaebc814019c61216a4f8582751f
stefann9 <105439203+stefann9@users.noreply.github.com>
2024-04-28 21:12:38 +0300

    add stlye to InputField & SelectField

--- src/components/formComponents/InputField.jsx
+++ src/components/formComponents/InputField.jsx
@@ -1,12 +1,13 @@
 function InputField({ id, name, label, type, value, onChange, required, ...otherProps }) {
   return (
-    <div>
-      <label htmlFor={id}>
+    <div className="mb-3">
+      <label htmlFor={id} className="form-label">
         {label}
         {required ? "*" : ""}:
       </label>
       <input
         type={type}
+        className={type === "checkbox" ? "form-check-input" : "form-control"}
         id={id}
         name={name}
         value={value}
--- src/components/formComponents/SelectField.jsx
+++ src/components/formComponents/SelectField.jsx
@@ -1,8 +1,8 @@
 function SelectField({ id, name, label, value, options, onChange, required }) {
   return (
-    <div>
-      <label htmlFor={id}>{label}:</label>
-      <select id={id} name={name} value={value} onChange={onChange} required={required}>
+    <div className="form-group">
+      <label className="form-label" htmlFor={id}>{label}:</label>
+      <select className="form-select" id={id} name={name} value={value} onChange={onChange} required={required}>
         {options.map((option, i) => (
           <option key={option.value} value={option.value}>
             {option.label}
