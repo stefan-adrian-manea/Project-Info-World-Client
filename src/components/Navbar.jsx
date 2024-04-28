0294d0e612b01784f7ea9f2c0efb9d0e9e337fcb
stefann9 <105439203+stefann9@users.noreply.github.com>
2024-04-28 20:39:52 +0300

    add bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,26 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Acasa</Link>
-        </li>
-        <li>
-          <Link to="/clients-add">Adaugare client</Link>
-        </li>
-        <li>
-          <Link to="/clients-list">Clienti</Link>
-        </li>
-        <li>
-          <Link to="/appointments">Programari</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-6 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/clients-add">Add Client</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/clients-list">Clients</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/appointments">Appointments</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
