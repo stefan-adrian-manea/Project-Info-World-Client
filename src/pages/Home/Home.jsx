197e3e666a9095456799e2a3127e0ec548e85d94
stefann9 <105439203+stefann9@users.noreply.github.com>
2024-04-29 23:26:52 +0300

    add basic Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div>
+      <h1 className="title">Internship 2024 Info World</h1>
+      <div className="buttons-container">
+        <Link to="/clients-add" className="button double-width">
+          <div className="button double-width">Add new client</div>
+        </Link>
+        <Link className="button" to="/clients-list">
+          <div>Clients List</div>
+        </Link>
+        <Link className="button" to="/appointments">
+          <div>Appointments List</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
